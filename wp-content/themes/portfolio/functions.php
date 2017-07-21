53311bca008bb047579cfe3ca472a9b9026a7ee5
Vladimir__Lipodat <sibi_7@mail.ru>
2017-07-22 00:32:35 +0300

    Connected styles, scripts, fonts

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * artcraft functions and definitions
+ * lipodat functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package artcraft
+ * @package lipodat
  */
 
-if ( ! function_exists( 'artcraft_setup' ) ) :
-function artcraft_setup() {
+if ( ! function_exists( 'lipodat_setup' ) ) :
+function lipodat_setup() {
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 	// Let WordPress manage the document title.
@@ -17,7 +17,7 @@ function artcraft_setup() {
 	add_theme_support( 'post-thumbnails' );
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'artcraft' ),
+		'primary' => esc_html__( 'Primary', 'lipodat' ),
 	) );
 	// Switch default core markup for search form, comment form, and comments
 	add_theme_support( 'html5', array(
@@ -31,40 +31,39 @@ function artcraft_setup() {
 	add_theme_support( 'customize-selective-refresh-widgets' );
 }
 endif;
-add_action( 'after_setup_theme', 'artcraft_setup' );
+add_action( 'after_setup_theme', 'lipodat_setup' );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-//function artcraft_widgets_init() {
+//function lipodat_widgets_init() {
 //	register_sidebar( array(
-//		'name'          => esc_html__( 'Sidebar', 'artcraft' ),
-//		'id'            => 'artcraft-sidebar',
-//		'description'   => esc_html__( 'Add widgets here.', 'artcraft' ),
+//		'name'          => esc_html__( 'Sidebar', 'lipodat' ),
+//		'id'            => 'lipodat-sidebar',
+//		'description'   => esc_html__( 'Add widgets here.', 'lipodat' ),
 //		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 //		'after_widget'  => '</section>',
 //		'before_title'  => '<h2 class="widget-title">',
 //		'after_title'   => '</h2>',
 //	) );
 //}
-//add_action( 'widgets_init', 'artcraft_widgets_init' );
+//add_action( 'widgets_init', 'lipodat_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function artcraft_scripts() {
+function lipodat_scripts() {
     /*
      * Подключаем стили:
      * Аргументы:
      * 1) название стиля (может быть любым)
      * 2) путь к файлу
      */
-    // для локальных стилей
-	wp_enqueue_style( 'artcraft-style', get_stylesheet_uri() . '/css/style.css' );
-    // для внешних стилей
-	//wp_enqueue_style( 'artcraft-style', 'http://domain.com/style.css' );
+    wp_enqueue_style( 'lipodat-fonts', 'https://fonts.googleapis.com/css?family=Inconsolata:400,700|Roboto:100,400,700" rel="stylesheet' );
+	wp_enqueue_style( 'lipodat-style', get_template_directory_uri() . '/css/style.min.css' );
+
     /*
      * Подключаем скрипты:
      * Аргументы:
@@ -74,7 +73,13 @@ function artcraft_scripts() {
      * 4) версия (оставляем пустые кавычки)
      * 5) подключение в футере (true = да, false = нет)
      */
-	wp_enqueue_script( 'artcraft-script', get_template_directory_uri() . '/js/script.js', [], '', true );
+	wp_enqueue_script( 'lipodat-jquery', get_template_directory_uri() . '/js/jquery.min.js', [], '', true );
+    wp_enqueue_script( 'lipodat-bootrsap', get_template_directory_uri() . '/js/bootstrap.min.js', [], '', true );
+    wp_enqueue_script( 'lipodat-validation', get_template_directory_uri() . '/js/validator.min.js', [], '', true );
+	wp_enqueue_script( 'lipodat-jquery.waypoints', get_template_directory_uri() . '/js/jquery.waypoints.js', [], '', true );
+	wp_enqueue_script( 'lipodat-parallax', get_template_directory_uri() . '/js/parallax.min.js', [], '', true );
+	wp_enqueue_script( 'lipodat-script', get_template_directory_uri() . '/js/scripts.js', [], '', true );
+
     /*
      * Добавляем возможность отправлять AJAX-запросы к скриптам
      * Аргументы:
@@ -82,14 +87,14 @@ function artcraft_scripts() {
      * 2) название объекта, к которому будем обращаться в файле скрипта
      * 3) элементы объекта, которые нам нужны (путь к обработчику аякса, путь к папке темы)
      */
-    wp_localize_script( 'artcraft-script', 'myajax',
+    wp_localize_script( 'lipodat-script', 'myajax',
         [
             'url' => admin_url( 'admin-ajax.php' ),
             'template' => get_template_directory_uri()
         ]
     );
 }
-add_action( 'wp_enqueue_scripts', 'artcraft_scripts' );
+add_action( 'wp_enqueue_scripts', 'lipodat_scripts' );
 
 /**
  * Load custom functions
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -33,8 +33,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
 
-<?php wp_head(); ?>
 </head>
 
 <body class="index">
